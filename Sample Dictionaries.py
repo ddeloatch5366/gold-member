1f109f462feae4a1a5a5fb9e9507eb9e20c0a7fe
Donnell Deloatch <d.deloatch@gmail.com>
2023-04-04 13:44:11 +0000

    What's Your Name 02

---
+++ Sample Dictionaries.py
@@ -0,0 +1,14 @@
+weights = dict(kevin=160, bob=240, kayla=135
+
+ages   =  { 'kevin': 59, 'alex': 29, 'bob': 40 }
+
+
+ages ['kevin']
+
+ages ['billy']
+
+del ages ['kevin']
+
+my_str .lower()
+
+my_str .upper()
